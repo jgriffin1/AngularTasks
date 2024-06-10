0ff7e60d10ef3f9b19f5a5d3c615b755ce3f5bcd
Joshua Griffin <josh.griff00@gmail.com>
2024-06-09 21:43:31 -0500

    Get, update, delete tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskComponent } from './components/task/task.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, TaskComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: string = 'Task Tracker';
 }
---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task'
+import { NgFor } from '@angular/common';
+import { TaskItemComponent } from '../task-item/task-item.component';
+import { TaskService } from '../../services/task.service';
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [NgFor, TaskItemComponent],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (
+      this.tasks = this.sortTasks(tasks)
+    ));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe((deletedTask) => (
+      this.tasks = this.tasks.filter(t => t.id !== deletedTask.id)
+    ));
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+
+    this.taskService.updateTask(task).subscribe((changedTask : Task) => {
+      this.tasks = this.tasks.filter(t => t.id !== changedTask.id);
+      this.tasks.push(changedTask);
+      this.tasks = this.sortTasks(this.tasks);
+    });
+  }
+
+  private sortTasks(tasks: Task[]): Task[] {
+    return tasks.sort(function(a: Task,b: Task) {
+      if(!a.id || !b.id) return 0;
+      return a.id - b.id
+    })
+  }
+}
